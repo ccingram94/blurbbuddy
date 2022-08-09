09fb5e06cba06ab5f513e506ef7fc76779fc3296
Constancegit status <constanceingram94@gmail.com>
2022-08-09 17:26:23 -0600

    css

--- components/subscriptionoptions.js
+++ components/subscriptionoptions.js
@@ -4,18 +4,20 @@ import Image from 'next/image'
 export default function SubscriptionOptions() {
     return (
         <div className="w-screen bg-transparent flex flex-row flex-wrap justify-center">
-            <div className="bg-gradient-to-bl from-purple-900 to-indigo-900 rounded-xl p-2 m-4">
-                <h3 className="p-2 text-purple-200 text-center font-bold text-3xl">Name Buddy</h3>
-                <p className="p-2 text-purple-200">generate the perfect character name</p>
+            <div className="bg-purple-200 bg-opacity-90 hover:bg-gradient-to-bl from-purple-900 to-indigo-900 text-purple-900 hover:text-purple-200 rounded-xl p-2 m-4 transition-all">
+                <h3 className="p-4 text-center font-bold text-3xl">Name Buddy</h3>
+                <p className="p-2">generate the perfect character name</p>
             </div>
-            <div className="bg-gradient-to-bl from-purple-900 to-indigo-900 rounded-xl p-2 m-4">
-                <h3 className="p-2 text-purple-200 text-center font-bold text-3xl text-purple-600 hover:text-rose-100 transition-all">Blurb Buddy</h3>
-                <p className="p-2 text-purple-200">generate book blurbs in a snap!</p>
+            <div className="bg-purple-200 hover:bg-gradient-to-bl from-purple-900 to-indigo-900 text-purple-900 hover:text-purple-200  rounded-xl p-2 m-4 transition-all">
+                <h3 className="p-4 hover:text-purple-200 text-center font-bold text-3xl">Blurb Buddy</h3>
+                <p className="p-2">generate book blurbs in a snap!</p>
             </div>
-            <div className="bg-gradient-to-bl from-purple-900 to-indigo-900 rounded-xl p-2 m-4">
-                <h3 className="p-2 text-purple-200 text-center font-bold text-3xl">Plot Buddy</h3>
-                <p className="p-2 text-purple-200">generate plot ideas that inspire you</p>
+            <div className="bg-purple-200 hover:bg-gradient-to-bl from-purple-900 to-indigo-900 text-purple-900 hover:text-purple-200  rounded-xl p-2 m-4 transition-all">
+                <h3 className="p-4 hover:text-purple-200 text-center font-bold text-3xl">Plot Buddy</h3>
+                <p className="p-2">generate plot ideas that inspire you</p>
             </div>
+
+
         </div>
     )
 }
