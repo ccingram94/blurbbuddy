852bb0a05de8b13b9a7f1afc6814549365c16a0a
Constancegit status <constanceingram94@gmail.com>
2022-08-09 21:57:21 -0600

    added api request for name generator

--- pages/api/namerequest.js
+++ pages/api/namerequest.js
@@ -1,9 +1,23 @@
+import { Configuration, OpenAIApi } from "openai";
 
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const querytext = req.query.text;
-        res.status(200).json({ text: querytext })
-    } else {
-        res.status(401).send({ message: 'Unauthorized' })
-    }
-}
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_SECRET_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+    const enteredtext = req.body;
+    const query = "Generate ten names for a person with this description: " + enteredtext + ".  They should not be famous names.   Use baby name books as a source.  Half should be unusual names.  Half should be common names.";
+    console.log(query)
+    console.log(enteredtext)
+        const response = await openai.createCompletion({
+            model: 'text-davinci-002',
+            prompt: query,
+            temperature: 0,
+            max_tokens: 100,
+        });
+        const textresponse = response.data.choices[0]
+        console.log(textresponse)
+        res.status(200).json({ textresponse })
+    } 
--- pages/namebuddy.js
+++ pages/namebuddy.js
@@ -1,10 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SubscriptionOptions from '../components/subscriptionoptions'
 
 export default function NameBuddy() {
   const [ text, setText ] = useState('');
+  const [ results, setResults ] = useState('')
+  const [ generating, setGenerating ] = useState(false);
+
+
+
+  const submitData = async(e) => {
+    e.preventDefault();
+    setGenerating(true);
+    const body = text;
+    const response = await fetch(`/api/namerequest`, {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+     });
+     const data = await await response.json();
+     const resultdata = data.textresponse.text;
+     setResults(resultdata);
+     setGenerating(false);
+    } 
+
   return (
     <div>
       <Head>
@@ -17,18 +37,24 @@ export default function NameBuddy() {
         <h1 className="text-center text-4xl md:text-6xl lg:text-7xl xl:text-8xl p-4 font-bold text-purple-100">
           Name Buddy 📝
         </h1>
-        <div className="flex flex-row flex-wrap">
+        <div className="flex flex-col flex-wrap">
           <div className="bg-purple-100 bg-opacity-70 rounded-xl m-2 text-center flex flex-col justify-center m-4 p-4">
             <div>
               <p className="text-xl lg:text-2xl text-purple-800 font-bold p-2 m-4">Describe your character below and Name Buddy will generate 10 names for you!</p>
             </div>
-            <div className="flex flex-col justify-center items-center p-2 m-4">
+            <form className="flex flex-col justify-center items-center p-2 m-4">
               <textarea type="text" onChange={(e) => setText(e.target.value)} className="p-2 rounded-xl m-2 text-purple-900 min-h-full min-w-full focus:outline-none"></textarea>
-              <button className="m-2 bg-white bg-opacity-90 text-xl max-w-fit rounded-xl p-2 text-purple-800">Generate Names</button>
-            </div>
+              <button onClick={(e) => submitData(e)} className="m-2 bg-white bg-opacity-90 text-xl max-w-fit rounded-xl p-2 text-purple-800">Generate Names</button>
+            </form>
           </div>
           <div className="flex flex-col jsutify-center p-2 m-4 bg-purple-100 bg-opacity-70 rounded-xl">
             <h3 className="text-xl font-bold p-2 m-4 text-purple-600">Results:</h3>
+            {generating && <p className="p-2 m-4 text-purple-600">generating names...</p>}
+            {results && !generating &&
+              <div>
+                <p className="p-2 m-4 text-purple-600 text-xl">{results}</p>
+              </div>
+            }
           </div>
         </div>
       </main>
